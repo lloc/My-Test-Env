ee65fb627afbc2cfb3a1236f6e317c7845619d51
Dennis Ploetner <re@lloc.de>
2011-10-05 17:35:59 +0200

    MyTestEnv.php added

---
+++ MyTestEnv.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+Plugin Name: My Test Env
+Description: Prepares my test environment
+Version: 0.1a
+Author: Dennis Ploetner 
+Author URI: http://lloc.de/
+*/
+
+/*
+Copyright 2011  Dennis Ploetner  (email : re@lloc.de)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+	register_post_type( 'acme_product',
+		array(
+			'labels' => array(
+				'name' => __( 'Products' ),
+				'singular_name' => __( 'Product' )
+			),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'products')
+		)
+	);
+}
+
+?>
