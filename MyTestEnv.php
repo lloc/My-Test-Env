d77677177367aa4027c1a7ff2a73d96d9ba3538b
Dennis Ploetner <re@lloc.de>
2011-10-10 20:12:50 +0200

    Custom taxonomy

--- MyTestEnv.php
+++ MyTestEnv.php
@@ -25,47 +25,34 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-add_action( 'init', 'create_post_type' );
 function create_post_type() {
-	register_post_type( 'acme_product',
-		array(
-			'labels' => array(
-				'name' => __( 'Products' ),
-				'singular_name' => __( 'Product' )
-			),
-			'public' => true,
-			'has_archive' => true,
-			'rewrite' => array('slug' => 'products')
-		)
+    register_post_type(
+        'acme_product',
+        array(
+            'labels' => array(
+                'name' => __( 'Products' ),
+                'singular_name' => __( 'Product' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'products')
+        )
 	);
 }
+add_action( 'init', 'create_post_type' );
 
-function my_msls_blog_collection_get( $objects ) {
-    $objects = array();
-    $blogs   = array( 1 => 'Override English', 2 => 'Override Deutsch');
-    foreach ( $blogs as $id => $description ) {
-        $details = get_blog_details( $id );
-        if ( is_object( $details ) ) {
-            $objects[$id] = new MslsBlog( $details, $description );
-        }
-    }
-    return $objects;
+function build_taxonomies() {  
+    register_taxonomy( 
+        'operating_system', 
+        'post', 
+        array( 
+            'hierarchical' => true, 
+            'label' => 'Operating System', 
+            'query_var' => true, 
+            'rewrite' => true 
+        ) 
+    );
 }
-//add_filter( 'msls_blog_collection_get', 'my_msls_blog_collection_get' );
-
-function mte_display_actions() {
-    global $wp_actions;
-    print_r( $wp_actions );
-}
-
-function mte_display_filter() {
-    global $wp_filter;
-    print_r( $wp_filter );
-}
-
-function mte_display_current_filter() {
-    global $wp_current_filter;
-    print_r( $wp_current_filter );
-}
+add_action( 'init', 'build_taxonomies', 0 );  
 
 ?>
