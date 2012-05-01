ede5f658f71054f8b75775bff44822efddcf4d34
Dennis Ploetner <re@lloc.de>
2012-05-01 11:00:30 +0200

    Added get_the_msls to menu-function

--- MyTestEnv.php
+++ MyTestEnv.php
@@ -75,7 +75,7 @@ add_filter( 'pre_get_posts', 'my_get_posts' );
 
 function my_custom_menu_item( $items, $args ) {
     print_r( $args );
-    if ( function_exists( 'get_the_msls' ) && $args->theme_location == 'primary-nav' ) {
+    if ( function_exists( 'get_the_msls' ) && $args->theme_location == 'primary' ) {
         $items .= '<li>' . get_the_msls() . '</li>';
     }
     return $items;
